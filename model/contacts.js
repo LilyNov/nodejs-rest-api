299a1efa6ca4a34f848520f9de01ed3ae224df4d
Liliia Novikova <kplily85@gmail.com>
2021-02-22 20:51:36 +0200

    add CRUD

--- model/contacts.js
+++ model/contacts.js
@@ -1,36 +1,36 @@
-const db = require("./db");
-const { v4: uuid } = require("uuid");
+const db = require('./db')
+const { v4: uuid } = require('uuid')
 // const fs = require("fs/promises");
 // const contacts = require("./contacts.json");
 
 const listContacts = async () => {
-  return db.get("contacts").value();
-};
+  return db.get('contacts').value()
+}
 
 const getContactById = async (contactId) => {
-  return db.get("contacts").find({ contactId }).value();
-};
+  return db.get('contacts').find({ contactId }).value()
+}
 
 const removeContact = async (contactId) => {
-  const [record] = db.get("contacts").remove({ contactId }).write();
-  return record;
-};
+  const [record] = db.get('contacts').remove({ contactId }).write()
+  return record
+}
 
 const addContact = async (body) => {
-  const contactId = uuid();
+  const contactId = uuid()
   const record = {
     contactId,
     ...body,
-  };
-  db.get("contacts").push(record).write();
-  return record;
-};
+  }
+  db.get('contacts').push(record).write()
+  return record
+}
 
 const updateContact = async (contactId, body) => {
-  const record = db.get("contacts").find({ contactId }).assign(body).value();
-  db.write();
-  return record.contactId ? record : null;
-};
+  const record = db.get('contacts').find({ contactId }).assign(body).value()
+  db.write()
+  return record.contactId ? record : null
+}
 
 module.exports = {
   listContacts,
@@ -38,4 +38,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-};
+}
