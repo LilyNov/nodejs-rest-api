12058a8b2be9f8fad6f0f0ff20f5a8c5652f25a7
Liliia Novikova <kplily85@gmail.com>
2021-03-11 11:07:46 +0200

    add mongoose.connection to db.js

--- model/db.js
+++ model/db.js
@@ -1,11 +1,31 @@
+const mongoose = require("mongoose");
 require("dotenv").config();
 const uriDb = process.env.URI_DB;
 
+const db = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connection to db");
+});
+
+mongoose.connection.on("connected", (err) => {
+  console.log(`Mongoose connection error ${err.message}`);
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose disconnected");
+});
+
 process.on("SIGINT", async () => {
-  const client = await db;
-  client.close();
-  console.log("Connection for db closed and app termination");
-  process.exit(1);
+  mongoose.connection.close(() => {
+    console.log("Connection for db closed and app termination");
+    process.exit(1);
+  });
 });
 
 module.exports = db;
