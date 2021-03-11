f29a00dd26cf5d177c1fe2eb082fa3b767777193
Liliia Novikova <kplily85@gmail.com>
2021-03-11 13:19:07 +0200

    add versionKey, timestamps in Schema

--- model/shemas/contact.js
+++ model/shemas/contact.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for a contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for a contact"],
+    },
+    email: {
+      type: String,
+      required: [true, "Set email for a contact"],
+      unique: true,
+    },
+    phone: {
+      type: Number,
+      required: [true, "Set name for a contact"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Set email for a contact"],
-    unique: true,
-  },
-  phone: {
-    type: Number,
-    required: [true, "Set name for a contact"],
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 contactSchema.virtual("id").get(function () {
   return this._id;
